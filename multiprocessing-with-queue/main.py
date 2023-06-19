ccbe9466d1b4c0dfc8800c6e5fea511d089c5d34
Marcello Romani <marcello.romani.enit@gmail.com>
2023-06-19 21:24:32 +0100

    count processed message, add class name to method in log message

--- multiprocessing-with-queue/main.py
+++ multiprocessing-with-queue/main.py
@@ -10,13 +10,16 @@ class SimpleMsgSource(MsgSource):
         """
         :param n: how many messages to produce
         """
+        log_info("SimpleMsgSource.__init__", f"n={n}")
         self._n = n
 
     def get_msg(self):
+        log_info("SimpleMsgSource.get_msg", "")
         for i in range(self._n):
             yield self.create_msg(i)
 
     def create_msg(self, i: int):
+        log_info("SimpleMsgSource.create_msg", str(i))
         return {
             "duration_s": 2,
             "msg_id": i,
@@ -24,12 +27,21 @@ class SimpleMsgSource(MsgSource):
 
 
 class SimpleMsgSink(MsgSink):
+
+    def __init__(self):
+        log_info("SimpleMsgSink.__init__", "")
+        self._processed_message = 0
+
+    def __del__(self):
+        log_info("SimpleMsgSink.__del__", f"Processed {self._processed_message} messages.")
+
     def process_msg(self, msg):
-        log_info("process_msg", "start")
+        log_info("SimpleMsgSink.process_msg", "start")
         duration_s = msg["duration_s"]
-        log_info("process_msg", f"Processing {msg}")
+        log_info("SimpleMsgSink.process_msg", f"Processing {msg}")
         sleep(duration_s)
-        log_info("process_msg", "end")
+        self._processed_message += 1
+        log_info("SimpleMsgSink.process_msg", "end")
 
 
 def message_factory(num_of_msg_to_create: int, queue_max_size: int, queue_get_and_put_timeout_s: int, worker_count: int):
