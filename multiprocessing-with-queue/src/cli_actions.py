450c8130a75f01c04430e69e15ac9833ba55fe6e
Marcello Romani <marcello.romani.enit@gmail.com>
2023-06-19 21:29:28 +0100

    refactor: move process setup in separate file

---
+++ multiprocessing-with-queue/src/cli_actions.py
@@ -0,0 +1,62 @@
+from time import sleep
+
+from src.log import log_info
+from src.process_manager import MsgSource, MsgSink
+from src.process_manager import ProcessManager
+
+
+class SimpleMsgSource(MsgSource):
+    def __init__(self, n: int):
+        """
+        :param n: how many messages to produce
+        """
+        log_info("SimpleMsgSource.__init__", f"n={n}")
+        self._n = n
+
+    def get_msg(self):
+        log_info("SimpleMsgSource.get_msg", "")
+        for i in range(self._n):
+            yield self.create_msg(i)
+
+    def create_msg(self, i: int):
+        log_info("SimpleMsgSource.create_msg", str(i))
+        return {
+            "duration_s": 2,
+            "msg_id": i,
+        }
+
+
+class SimpleMsgSink(MsgSink):
+
+    def __init__(self):
+        METHOD = "SimpleMsgSink.__init__"
+        log_info(METHOD, "")
+        self._processed_message = 0
+
+    def __del__(self):
+        METHOD = "SimpleMsgSink.__del__"
+        log_info(METHOD, f"Processed {self._processed_message} messages.")
+
+    def process_msg(self, msg):
+        METHOD = "SimpleMsgSink.process_msg"
+        log_info(METHOD, "start")
+        duration_s = msg["duration_s"]
+        log_info(METHOD, f"Processing {msg}")
+        sleep(duration_s)
+        self._processed_message += 1
+        log_info(METHOD, "end")
+
+
+def message_factory(num_of_msg_to_create: int, queue_max_size: int, queue_get_and_put_timeout_s: int,
+                    worker_count: int):
+    """
+    Creates and run the whole "message producer / queue / consumers" setup based on the provided parameters
+    :param num_of_msg_to_create: the producer will create and (try to) enqueue this many messages before terminating
+    :param queue_max_size: maximum number of messages the queue can hold at any given time
+    :param queue_get_and_put_timeout_s: q.put() and q.get() operations will wait at most these many seconds before timing out
+    :param worker_count: number of processes reading messages off the queue and executing/processing them
+    """
+    src = SimpleMsgSource(num_of_msg_to_create)
+    dst = SimpleMsgSink()
+    p = ProcessManager(queue_max_size, queue_get_and_put_timeout_s)
+    p.process(src, dst, worker_count)
