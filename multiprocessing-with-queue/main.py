aac76f7be30b7090e156e645a9a26d949f1eb092
Marcello Romani <marcello.romani.enit@gmail.com>
2023-06-19 21:43:15 +0100

    feature: print elapsed time in seconds

--- multiprocessing-with-queue/main.py
+++ multiprocessing-with-queue/main.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from time import perf_counter_ns
 
 from src.cli_actions import message_factory
 
@@ -41,12 +42,16 @@ def main():
     parser = opt_setup()
     args = parser.parse_args()
 
+    t_start = perf_counter_ns()
     message_factory(
         num_of_msg_to_create=args.msg_count,
         queue_max_size=args.queue_size,
         queue_get_and_put_timeout_s=args.queue_timeout,
         worker_count=args.worker_count
     )
+    t_end = perf_counter_ns()
+    t_elapsed_sec = (t_end - t_start) / 1_000_000_000
+    print(f"Elapsed: {t_elapsed_sec}")
 
 
 if __name__ == "__main__":
