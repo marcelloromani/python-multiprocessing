8aa6054fa4872fc9d65ff7d161f26cb27ab8be94
Marcello Romani <marcello.romani.enit@gmail.com>
2023-06-19 21:24:00 +0100

    add class to method name, refactor: DRY method string in log messages

--- multiprocessing-with-queue/src/process_manager.py
+++ multiprocessing-with-queue/src/process_manager.py
@@ -25,47 +25,55 @@ class ProcessManager:
         :param queue_max_size: maximum size of the internal queue
         :param queue_timeout_s: how long to wait for a q.get() or q.put() to succeed before declaring it failed
         """
+        METHOD = "ProcessManager.__init__"
+
         self._q = Queue(queue_max_size)
         self._queue_timeout = queue_timeout_s
-        log_info("ProcessManager", f"queue_max_size: {queue_max_size} queue_timeout_s: {queue_timeout_s}")
+        log_info(METHOD, f"queue_max_size: {queue_max_size} queue_timeout_s: {queue_timeout_s}")
 
     @staticmethod
     def _enqueue_msg(q: Queue, msg_type: str, msg: str, timeout: int):
+        METHOD: str = "ProcessManager._enqueue_msg"
+
         try:
-            log_info("_enqueue_msg", f"Trying to enqueue {msg_type} {msg}")
+            log_info(METHOD, f"Trying to enqueue {msg_type} {msg}")
             q.put((msg_type, msg), block=True, timeout=timeout)
-            log_info("_enqueue_msg", f"Enqueued {msg_type} {msg}")
+            log_info(METHOD, f"Enqueued {msg_type} {msg}")
         except TimeoutError as e:
-            log_info("_enqueue_msg", f"TimeoutError: {e}")
+            log_info(METHOD, f"TimeoutError: {e}")
             raise e
         except queue.Full as e:
-            log_info("_enqueue_msg", f"queue.Full: {e}")
+            log_info(METHOD, f"queue.Full: {e}")
             raise e
         except Exception as e:
-            log_info("_enqueue_msg", f"Exception: {e}")
+            log_info(METHOD, f"Exception: {e}")
             raise e
 
     @staticmethod
     def _dequeue_msg(q: Queue, timeout: int):
+        METHOD: str = "ProcessManager._dequeue_msg"
+
         try:
-            log_info("_dequeue_msg", "trying to dequeue message")
+            log_info(METHOD, "trying to dequeue message")
             msg_type, msg = q.get(block=True, timeout=timeout)
-            log_info("_dequeue_msg", f"dequeued {msg_type} {msg}")
+            log_info(METHOD, f"dequeued {msg_type} {msg}")
             return msg_type, msg
         except TimeoutError as e:
-            log_info("_dequeue_msg", f"TimeoutError: {e}")
+            log_info(METHOD, f"TimeoutError: {e}")
             raise e
         except queue.Empty as e:
-            log_info("_dequeue_msg", f"queue.Empty: {e}")
+            log_info(METHOD, f"queue.Empty: {e}")
             raise e
 
     def process(self, msg_source: MsgSource, msg_sink: MsgSink, worker_count: int):
-        log_info("process", "start")
+        METHOD: str = "ProcessManager.process"
+
+        log_info(METHOD, "start")
 
         # create worker pool
         workers = []
         for i in range(worker_count):
-            log_info("process", f"Creating worker process {i}")
+            log_info(METHOD, f"Creating worker process {i}")
             p = Process(target=self._dequeue_and_process_msg, args=(msg_sink,))
             workers.append(p)
             p.start()
@@ -76,13 +84,15 @@ class ProcessManager:
         self._enqueue_msg(self._q, self.MSG_TYPE_QUIT, "", self._queue_timeout)
 
         for p in workers:
-            log_info("process", "Joining worker process")
+            log_info(METHOD, "Joining worker process")
             p.join()
 
-        log_info("process", "end")
+        log_info(METHOD, "end")
 
     def _dequeue_and_process_msg(self, msg_sink: MsgSink):
-        log_info("_dequeue_and_process_msg", "start")
+        METHOD: str = "ProcessManager._dequeue_and_process_msg"
+
+        log_info(METHOD, "start")
 
         terminate = False
 
@@ -93,14 +103,14 @@ class ProcessManager:
             if msg_type:
                 if msg_type == self.MSG_TYPE_USER:
                     try:
-                        log_info("_dequeue_and_process_msg", f"processing {msg_type} {msg}")
+                        log_info(METHOD, f"processing {msg_type} {msg}")
                         msg_sink.process_msg(msg)
                     except Exception as e:
-                        log_info("_dequeue_and_process_msg", f"Error while processing message: {e}")
+                        log_info(METHOD, f"Error while processing message: {e}")
                 elif msg_type == self.MSG_TYPE_QUIT:
                     self._enqueue_msg(self._q, self.MSG_TYPE_QUIT, "", self._queue_timeout)
                     terminate = True
                 else:
                     raise ValueError(f"Unexpected message type {msg_type}")
 
-        log_info("_dequeue_and_process_msg", "end")
+        log_info(METHOD, "end")
