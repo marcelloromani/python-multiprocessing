57530a66234fb78c9f6f3f44ac2d6314bcd2a12e
Marcello Romani <marcello.romani.enit@gmail.com>
2023-07-08 20:29:21 +0100

    added some initial unit tests

---
+++ multiprocessing-with-queue/tests/test_log.py
@@ -0,0 +1,17 @@
+import pytest
+
+from src.log import log_setup
+
+
+class TestLog:
+
+    def test_log_setup_should_raise_exception_if_log_level_is_random_string(self):
+        with pytest.raises(ValueError):
+            log_setup("foo")
+
+    def test_log_setup_should_raise_exception_if_log_level_is_lowercase_log_level(self):
+        with pytest.raises(ValueError):
+            log_setup("debug")
+
+    def test_log_setup_should_accept_uppercase_log_devel(self):
+        log_setup("DEBUG")
