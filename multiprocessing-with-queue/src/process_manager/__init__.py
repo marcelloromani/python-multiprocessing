ff98775df256ff9bd8e11ded9c873a7085e1ba31
Marcello Romani <marcello.romani.enig@gmail.com>
2023-07-23 11:32:25 +0100

    refactor: split out enqueue/dequeue functionality from process manager

--- multiprocessing-with-queue/src/process_manager/__init__.py
+++ multiprocessing-with-queue/src/process_manager/__init__.py
@@ -1,2 +1,4 @@
 from .interfaces import MsgProducer, MsgConsumer
+from .msg_dequeuer import MsgDequeuer
+from .msg_enqueuer import MsgEnqueuer
 from .process_manager import ProcessManager
---
+++ multiprocessing-with-queue/src/process_manager/msg_enqueuer.py
@@ -0,0 +1,43 @@
+import logging
+import queue
+from multiprocessing import Queue
+from time import sleep
+
+
+class MsgEnqueuer:
+    logger = logging.getLogger()
+
+    def __init__(self, timeout: float, max_attempts: int, wait_between_attempts: float):
+        """
+        :param timeout: queue.put(): how long to wait before timing out
+        :param max_attempts: how many times to retry on timeout
+        :param wait_between_attempts: before trying another put() after timeout, wait these many seconds
+        """
+        self._timeout = timeout
+        self._max_attempts = max_attempts
+        self._wait_between_attempts = wait_between_attempts
+
+    def put(self, msg_queue: Queue, msg_type: str, msg: str):
+        attempts: int = 0
+
+        while attempts < self._max_attempts:
+            try:
+                self.logger.debug("Trying to enqueue %s %s attempts=%d", msg_type, msg, attempts)
+
+                attempts += 1
+                msg_queue.put((msg_type, msg), block=True, timeout=self._timeout)
+
+                self.logger.debug("Enqueued %s %s after %d attempts", msg_type, msg, attempts)
+                break
+            except TimeoutError as ex:
+                self.logger.error("TimeoutError: %s", ex)
+                raise
+            except queue.Full as ex:
+                self.logger.debug("queue.Full: %s attempts: %d", ex, attempts)
+
+                if attempts < self._max_attempts:
+                    self.logger.debug("Sleeping %f sec before next attempt", self._wait_between_attempts)
+                    sleep(self._wait_between_attempts)
+                else:
+                    self.logger.error("Reached max attempts %d", self._max_attempts)
+                    raise
