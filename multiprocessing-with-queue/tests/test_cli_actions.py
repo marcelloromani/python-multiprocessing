118242449f24eec8c2aa626d05fab663fea290d5
Marcello Romani <marcello.romani.enig@gmail.com>
2023-08-26 16:37:55 +0100

    test: also cover cli actions

--- multiprocessing-with-queue/tests/test_cli_actions.py
+++ multiprocessing-with-queue/tests/test_cli_actions.py
@@ -1,5 +1,10 @@
+import pytest
+
 from src.cli_actions import SimpleMsgConsumer
 from src.cli_actions import SimpleMsgProducer
+from src.cli_actions import run_session
+from src.cli_actions import run_single
+from src.config import Config
 
 
 class TestSimpleMsgProducer:
@@ -20,3 +25,55 @@ class TestSimpleMsgConsumer:
         assert obj.processed_message_count == 0
         obj.process_msg({"duration_s": 0})
         assert obj.processed_message_count == 1
+
+
+class TestRuns:
+
+    def test_should_run_single(self):
+        config = Config()
+        config.msg_count = 1
+        config.task_duration_sec = 0
+        config.queue_max_size = 1
+        config.consumer_count = 1
+        config.queue_put_timeout_sec = 0
+        config.queue_full_max_attempts = 5
+        config.queue_full_wait_sec = 1
+        config.queue_get_timeout_sec = 1
+        config.queue_empty_max_attempts = 5
+        config.queue_empty_wait_sec = 0
+        run_single(config)
+
+    def test_should_run_session(self):
+        config = Config()
+        config.msg_count = 1
+        config.task_duration_sec = 0
+        config.queue_max_size = 1
+        config.consumer_count = 1
+        config.queue_put_timeout_sec = 0
+        config.queue_full_max_attempts = 5
+        config.queue_full_wait_sec = 1
+        config.queue_get_timeout_sec = 1
+        config.queue_empty_max_attempts = 5
+        config.queue_empty_wait_sec = 0
+        consumer_min = 1
+        consumer_max = 2
+        consumer_step = 1
+        run_session(config, consumer_min, consumer_max, consumer_step)
+
+    def test_should_fail_if_run_session_and_consumer_step_zero(self):
+        config = Config()
+        config.msg_count = 1
+        config.task_duration_sec = 0
+        config.queue_max_size = 1
+        config.consumer_count = 1
+        config.queue_put_timeout_sec = 0
+        config.queue_full_max_attempts = 5
+        config.queue_full_wait_sec = 1
+        config.queue_get_timeout_sec = 1
+        config.queue_empty_max_attempts = 5
+        config.queue_empty_wait_sec = 0
+        consumer_min = 1
+        consumer_max = 2
+        consumer_step = 0
+        with pytest.raises(ValueError):
+            run_session(config, consumer_min, consumer_max, consumer_step)
