dcb5d78d42388de7687c267c9bb316dd0cc21cea
Marcello Romani <marcello.romani.enig@gmail.com>
2023-08-27 20:00:52 +0100

    add generic retry logic for enqueue/dequeue

--- multiprocessing-with-queue/src/process_manager/msg_processor.py
+++ multiprocessing-with-queue/src/process_manager/msg_processor.py
@@ -1,4 +1,8 @@
+import logging
+
+import queue
 class MsgProcessor:
+    logger = logging.getLogger()
 
     def __init__(self, timeout: float = 0, max_attempts: int = 1, wait_between_attempts: float = 0):
         """
@@ -21,3 +25,25 @@ class MsgProcessor:
     @property
     def wait_between_attempts(self):
         return self._wait_between_attempts
+
+    def _run_with_retry(self, func, *args, **kwargs):
+        attempts: int = 0
+
+        while attempts < self.max_attempts:
+            try:
+                attempts += 1
+                return func(*args, **kwargs)
+            except TimeoutError as ex:
+                self.logger.error("TimeoutError: %s Attempts: %d", ex, attempts)
+                if attempts >= self.max_attempts:
+                    raise
+
+            except queue.Full as ex:
+                self.logger.error("Queue Full: %s Attempts: %d", ex, attempts)
+                if attempts >= self.max_attempts:
+                    raise
+
+            except queue.Empty as ex:
+                self.logger.error("Queue Empty: %s Attempts: %d", ex, attempts)
+                if attempts >= self.max_attempts:
+                    raise
---
+++ multiprocessing-with-queue/tests/test_msg_processor.py
@@ -0,0 +1,131 @@
+import queue
+
+import pytest
+from box import Box
+
+from src.process_manager.msg_processor import MsgProcessor
+
+
+class TestMsgProcessorRetryLogic:
+
+    @classmethod
+    def no_exception(cls, ctx: Box):
+        ctx.call_count += 1
+
+    @classmethod
+    def raise_timeout_once(cls, ctx: Box):
+        ctx.call_count += 1
+        if ctx.call_count == 1:
+            raise TimeoutError
+
+    @classmethod
+    def raise_queue_full_once(cls, ctx: Box):
+        ctx.call_count += 1
+        if ctx.call_count == 1:
+            raise queue.Full
+
+    @classmethod
+    def raise_queue_empty_once(cls, ctx: Box):
+        ctx.call_count += 1
+        if ctx.call_count == 1:
+            raise queue.Empty
+
+    @classmethod
+    def multiply(cls, value: int) -> int:
+        return value * 2
+
+    # happy path
+
+    def test_should_call_func_once_if_no_exception(self):
+        obj = MsgProcessor(
+            timeout=0,
+            max_attempts=1,
+            wait_between_attempts=1,
+        )
+        ctx = Box()
+        ctx.call_count = 0
+        obj._run_with_retry(self.no_exception, ctx)
+        assert ctx.call_count == 1
+
+    # timeout exception
+
+    def test_should_fail_if_only_one_attempt_and_timeout(self):
+        obj = MsgProcessor(
+            timeout=0,
+            max_attempts=1,
+            wait_between_attempts=0,
+        )
+        ctx = Box()
+        ctx.call_count = 0
+        with pytest.raises(TimeoutError):
+            obj._run_with_retry(self.raise_timeout_once, ctx)
+
+    def test_should_not_fail_if_two_attempts_and_timeout(self):
+        obj = MsgProcessor(
+            timeout=0,
+            max_attempts=2,
+            wait_between_attempts=0,
+        )
+        ctx = Box()
+        ctx.call_count = 0
+        obj._run_with_retry(self.raise_timeout_once, ctx)
+        assert ctx.call_count == 2
+
+    # queue full exception
+
+    def test_should_fail_if_only_one_attempt_and_queue_full(self):
+        obj = MsgProcessor(
+            timeout=0,
+            max_attempts=1,
+            wait_between_attempts=0,
+        )
+        ctx = Box()
+        ctx.call_count = 0
+        with pytest.raises(queue.Full):
+            obj._run_with_retry(self.raise_queue_full_once, ctx)
+
+    def test_should_not_fail_if_two_attempts_and_queue_full(self):
+        obj = MsgProcessor(
+            timeout=0,
+            max_attempts=2,
+            wait_between_attempts=0,
+        )
+        ctx = Box()
+        ctx.call_count = 0
+        obj._run_with_retry(self.raise_queue_full_once, ctx)
+        assert ctx.call_count == 2
+
+    # queue empty exception
+
+    def test_should_fail_if_only_one_attempt_and_queue_empty(self):
+        obj = MsgProcessor(
+            timeout=0,
+            max_attempts=1,
+            wait_between_attempts=0,
+        )
+        ctx = Box()
+        ctx.call_count = 0
+        with pytest.raises(queue.Empty):
+            obj._run_with_retry(self.raise_queue_empty_once, ctx)
+
+    def test_should_not_fail_if_two_attempts_and_queue_empty(self):
+        obj = MsgProcessor(
+            timeout=0,
+            max_attempts=2,
+            wait_between_attempts=0,
+        )
+        ctx = Box()
+        ctx.call_count = 0
+        obj._run_with_retry(self.raise_queue_empty_once, ctx)
+        assert ctx.call_count == 2
+
+    # return value form the wrapped function
+
+    def test_should_relay_func_return_value(self):
+        obj = MsgProcessor(
+            timeout=0,
+            max_attempts=1,
+            wait_between_attempts=0,
+        )
+        retval = obj._run_with_retry(self.multiply, 3)
+        assert retval == 6
